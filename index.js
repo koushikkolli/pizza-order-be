b38410472382f3d4cb9203e4bad8d6e22a761003
koushikkolli <31841404+koushikkolli@users.noreply.github.com>
2021-01-10 06:23:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -133,6 +133,7 @@ app.post("/register", async (req, res) => {
       }
       client.close();
     } catch (error) {
+      console.log(error)
       res.status(500).json({
         message: "Internal Server Error"
       });
